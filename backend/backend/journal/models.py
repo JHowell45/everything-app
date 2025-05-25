c1ce9ba7a051899fc1bf5a0102604be78aad4f14
= <jhowell45@googlemail.com>
2025-05-25 17:50:53 +0100

    removing app and starting again

--- backend/backend/journal/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class JournalEntry(models.Model):
-    entry_date = models.DateField(blank=False, null=False)
-    entry_text: str = models.TextField(blank=False, null=False)
-    created_at = models.DateTimeField(auto_now_add=True, null=False)
-    updated_at = models.DateTimeField(auto_now=True, null=False)
