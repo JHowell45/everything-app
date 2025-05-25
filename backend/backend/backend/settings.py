ec433137d6e2d4a71373a1c4494d1e8ebeb2ba6d
= <jhowell45@googlemail.com>
2025-05-25 17:49:32 +0100

    moving

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = environ.get("ALLOWED_HOSTS", "").split(",")
 # Application definition
 
 INSTALLED_APPS = [
+    'journal.apps.JournalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
