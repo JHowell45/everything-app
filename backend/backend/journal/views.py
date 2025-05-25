4ea6a0cbdf5ab1d60918af2ac5c512b644f342fc
= <jhowell45@googlemail.com>
2025-05-25 18:19:42 +0100

    adding templates and urls data

--- backend/backend/journal/views.py
+++ backend/backend/journal/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import JournalEntry
 
 # Create your views here.
+class IndexView(generic.ListView):
+    model = JournalEntry
+    template_name = "journal/index.html"
+
+    def get_queryset(self):
+        return JournalEntry.objects.all()
