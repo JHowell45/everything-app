c6f9fc0b6b9788080ef1b2ec6110dae146477c2f
= <jhowell45@googlemail.com>
2025-05-26 16:35:28 +0100

    removing old views.

--- backend/backend/journal/views.py
+++ backend/backend/journal/views.py
@@ -19,26 +19,3 @@ class JournalEntryViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
-class IndexView(generic.ListView):
-    template_name = "journal/index.html"
-    context_object_name = "journal_entries"
-
-    def get_queryset(self):
-        return JournalEntry.objects.all()
-
-def new_entry(request):
-    if request.method == "POST":
-        form = EntryForm(request.POST)
-        if form.is_valid():
-            entry_date = timezone.now().date()
-            entry_text: str = form.cleaned_data.get("entry")
-            if entry := JournalEntry.objects.filter(entry_date=entry_date).first():
-                entry.entry_text += f"\n{entry_text}"
-                entry.save()
-            else:
-                JournalEntry.objects.create(entry_text=entry_text, entry_date = timezone.now())
-            return redirect("journal:index")
-        else:
-            return HttpResponse("Failed!")
-    else:
-        return HttpResponse("Not POST!")
