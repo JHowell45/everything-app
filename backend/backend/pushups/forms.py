5aadcf72f1c3f45e1c0c73e9e365f910d449e12a
= <jhowell45@googlemail.com>
2025-05-26 22:02:23 +0100

    fixing the form data and checks.

--- backend/backend/pushups/forms.py
+++ backend/backend/pushups/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from pygments.lexer import default
 
 
 class PushupForm(forms.Form):
     body_weight: float = forms.FloatField(label="weight", min_value=50)
     amount: int = forms.IntegerField(label="amount", min_value=0)
+    on_knees: bool = forms.BooleanField(label="on knee", required=False)
--- backend/backend/pushups/views.py
+++ backend/backend/pushups/views.py
@@ -3,6 +3,9 @@ from django.views import generic
 from django.shortcuts import redirect
 from pushups.forms import PushupForm
 from pushups.models import Pushup
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 # Create your views here.
@@ -11,12 +14,16 @@ class IndexView(generic.ListView):
     context_object_name = "pushups"
 
     def get_queryset(self):
-        return Pushup.objects.all().order_by("set_finished")
+        return Pushup.objects.all()
 
 
 def new_entry(request):
     if request.method == "POST":
         form = PushupForm(request.POST)
         if form.is_valid():
-            Pushup.objects.create(body_weight=form.cleaned_data["body_weight"], amount=form.cleaned_data["amount"])
+            logger.warning(form.cleaned_data)
+            Pushup.objects.create(body_weight=form.cleaned_data["body_weight"], amount=form.cleaned_data["amount"],
+                                  on_knees=form.cleaned_data["on_knees"] or False)
+        else:
+            logger.warning(form.errors)
     return redirect("pushups:index")
